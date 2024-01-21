818de206c672fd346dcd6b1c1519b6d3c5276e80
Tomvsa <tomasvillasecasaavedra@gmail.com>
2024-01-21 15:04:38 +0100

    2# migrates stores in json data

--- database/migrations/2024_01_16_182904_create_films_table.php
+++ database/migrations/2024_01_16_182904_create_films_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 return new class extends Migration
 {
@@ -11,6 +13,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Create a temporal table
         Schema::create('films', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
@@ -21,6 +24,7 @@ return new class extends Migration
             $table->string('img_url', 255);
             $table->timestamps();
         });
+
     }
 
     /**
@@ -28,6 +32,33 @@ return new class extends Migration
      */
     public function down(): void
     {
+    // Create a temporal table
+        Schema::create('films_copy', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->year('year');
+            $table->string('genre', 50);
+            $table->string('country', 30);
+            $table->integer('duration');
+            $table->string('img_url', 255);
+            $table->timestamps();
+        });
+
+        // Save data to a temporary JSON file
+        $data = DB::table('films')->get()->toArray();
+        $jsonData = json_encode($data);
+        File::put(storage_path('app/temporary_data.json'), $jsonData);
+        
+        // Read data from the temporary JSON file
+        $jsonData = File::get(storage_path('app/temporary_data.json'));
+        $data = json_decode($jsonData, true);
+        // Insert data into the original films table
+        DB::table('films_copy')->insert($data);
+
+        // Delete the temporary JSON file
+        File::delete(storage_path('app/temporary_data.json'));
+
+        // Drop  table
         Schema::dropIfExists('films');
     }
 };
